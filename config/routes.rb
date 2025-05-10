7696990d2a6638399c3d6e4aa2853da9826b6483
Sabreena Salomon <salomonbree@gmail.com>
2025-05-10 21:00:29 +0000

    edited href for show details

--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,28 @@ Rails.application.routes.draw do
   resources :directors
   
   # # CREATE
-  # post "/movies" => "movies#create" 
-  # get "/movies/new" => "movies#new"
+  # # post("/movies", { :controller => "movies", :action => "create" })
+  # post "/movies" => "movies#create", as: :movies
 
+  # # get("/movies/new", { :controller => "movies", :action => "new" })
+  # get "/movies/new" => "movies#new", as: :new_movie
+          
   # # READ
-  # get "/movies" => "movies#index" 
-  # get "/movies/:id" => "movies#show"
+  # # get("/movies", { :controller => "movies", :action => "index" })
+  # get "/movies" => "movies#index" # this one is already named as movies 
 
-  # # UPDATE
+  # # get("/movies/:id", { :controller => "movies", :action => "show" })
+  # get "/movies/:id" => "movies#show", as: :movie
   
-  # patch "/movies/:id" => "movies#update" 
-  # get "/movies/:id/edit" => "movies#edit"
+  # # UPDATE
+  # # patch("/movies/:id", { :controller => "movies", :action => "update" })
+  # patch "/movies/:id" => "movies#update" # this one is already named as movie
 
+  # # get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  # get "movies/:id/edit" => "movies#edit", as: :edit_movie 
+  
   # # DELETE
-   
-  # delete "/movies/:id" => "movies#destroy"
+  # # delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  # delete "movies/:id" => "movies#destroy" # this one is already named as movie
   # #------------------------------
 end
